d7a08f309106be16b6b2e17b1eb4eae961d8ac71
nruffin95 <nruffin95@gmail.com>
2022-03-28 18:29:22 -0500

    Added JS Functions and varibles

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?")
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+function fight() {
+    window.alert("The Fight has Begun!")
+}
+
+// fight();
