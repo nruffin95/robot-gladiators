41bea15cc0366a890bd0231bd080fff3dffb24bb
nruffin95 <nruffin95@gmail.com>
2022-03-30 17:55:07 -0500

    ad enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,21 @@
-var playerName = window.prompt("What is your robots name?");
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
-var enemyHealth = 10;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to fight or skip this battle? Enter 'FIGHT' OR 'SKIP' to choose.");
 };
-
-
 
 fight()
 
 enemyHealth = enemyHealth - playerAttack;
 playerHealth = playerHealth - enemyAttack;
-
-console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-)
 
 // if player choses to fight, then fight
 
@@ -65,5 +59,9 @@ console.log(
     // if no (false), ask question again by running fight() again
     else {
       fight();
+    }
+
+    for(var i = 0; i < enemyNames.length; i++) {
+      fight(enemyNames[i]);
     }
   }
